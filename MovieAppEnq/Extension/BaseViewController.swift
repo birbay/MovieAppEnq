255bed9e6eccbc81da3142b4c0ff36a6abe60c31
Bircan Bayram <bircanbayram16@gmail.com>
2020-07-25 22:21:57 +0300

    Initial

---
+++ MovieAppEnq/Extension/BaseViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BaseViewController.swift
+//  MovieAppEnq
+//
+//  Created by Birbay on 25.07.2020.
+//  Copyright © 2020 Birbay. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
